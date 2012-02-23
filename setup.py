c2317dfa3c1cea7535e78d847a1e79aae15c9854
Clayton Parker <clayton@sixfeetup.com>
2012-02-22 22:28:14 -0500

    initial skeleton

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='sixieskel.karl',
+      version=version,
+      description="Collection of templates for KARL development",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='karl python templer template',
+      author='Six Feet Up, Inc.',
+      author_email='info@sixfeetup.com',
+      url='http://github.com/sixfeetup/sixieskel.karl',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['sixieskel'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
