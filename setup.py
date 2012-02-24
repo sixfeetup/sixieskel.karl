4197bce08403a1202a50ca5c5ef6938466d4e9b5
Clayton Parker <clayton@sixfeetup.com>
2012-02-23 20:06:01 -0500

    clean up formatting

--- setup.py
+++ setup.py
@@ -13,31 +13,32 @@ long_description = (
     open('CHANGES.txt').read()
     + '\n')
 
-setup(name='sixieskel.karl',
-      version=version,
-      description="Collection of templates for KARL development",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='sixieskel.karl',
+    version=version,
+    description="Collection of templates for KARL development",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Programming Language :: Python",
-        ],
-      keywords='karl python templer template',
-      author='Six Feet Up, Inc.',
-      author_email='info@sixfeetup.com',
-      url='http://github.com/sixfeetup/sixieskel.karl',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir = {'': 'src'},
-      namespace_packages=['sixieskel'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'sixieskel.buildout',
-      ],
-      entry_points="""
-      [paste.paster_create_template]
-      sfu_karl_buildout = sixieskel.karl.template:KarlBuildout
-      """,
-      )
+    ],
+    keywords='karl python templer template',
+    author='Six Feet Up, Inc.',
+    author_email='info@sixfeetup.com',
+    url='http://github.com/sixfeetup/sixieskel.karl',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['sixieskel'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'sixieskel.buildout',
+    ],
+    entry_points="""
+    [paste.paster_create_template]
+    sfu_karl_buildout = sixieskel.karl.template:KarlBuildout
+    """,
+    )
